dfa85b6bdb64d9d554acfb306a80cdc86d26d0cb
pcanaleta <pcanaleta@ibecbarcelona.eu>
2026-01-30 12:36:10 +0100

    Update __init__.py

--- tanalysis/__init__.py
+++ tanalysis/__init__.py
@@ -7,3 +7,4 @@
 # .venv separados de proyecto
 # poetry update per actualitzar tots els paquets
 # poetry install
+# https://medium.com/@patrishaanneestrada/exploring-blob-detection-and-connected-components-for-image-analysis-a501e3adf68c
